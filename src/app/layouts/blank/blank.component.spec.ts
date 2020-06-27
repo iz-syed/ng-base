2e9fe11dcb2d8da42860da92686b46f6accb8ae0
iz-syed <mubarakcc7@gmail.com>
2020-06-28 02:13:52 +0530

    Revert "amend"
    This reverts commit ff359e01e2f7f07927576e0e30b2429e8e735188.

--- src/app/layouts/blank/blank.component.spec.ts
+++ src/app/layouts/blank/blank.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PagesComponent } from './pages.component';
+import { BlankComponent } from './blank.component';
 
-describe('PagesComponent', () => {
-  let component: PagesComponent;
-  let fixture: ComponentFixture<PagesComponent>;
+describe('BlankComponent', () => {
+  let component: BlankComponent;
+  let fixture: ComponentFixture<BlankComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PagesComponent ]
+      declarations: [ BlankComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PagesComponent);
+    fixture = TestBed.createComponent(BlankComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
